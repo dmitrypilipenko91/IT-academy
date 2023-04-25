1f4de794c63be4bb88b9ff9f1c59ad99133155c2
dmitrypilipenko91 <dmitrypilipenko91@gmail.com>
2023-04-26 03:12:19 +0500

    Create task3.html and script3.js for task#3

---
+++ homework#1/script3.js
@@ -0,0 +1,16 @@
+let message; 
+let login;
+
+switch ( login ) {
+    case "Сотрудник":
+        message == "Привет";
+        break;
+    case "Директор":
+        message == "Здравствуйте";
+        break;
+    case "":
+        message == "Нет логина";
+        break;
+    default:
+        message == "";    
+}
